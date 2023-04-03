551267780d8f7fd55ed4be1bdc06622a88ff5f5c
Adrian Hidayat <ahidayat@paypal.com>
2023-04-03 13:06:20 -0400

    userrole privilege stub

--- src/main/resources/02-create-crp-fineract-db-users.sh
+++ src/main/resources/02-create-crp-fineract-db-users.sh
@@ -3,7 +3,7 @@
 # The passwords for all these users should be provided as environment variables, for example, FINERACT_DATABASE_USER_<username-in-upper-case>_PASSWORD.
 set -e
 
-echo "----------Begin Database Setup---------- \n"
+echo "----------Begin Database Setup----------"
 
 check_and_create_userroles() {
   if [ -z $FINERACT_DATABASE_USERS ]; then
@@ -22,7 +22,16 @@ check_and_create_userroles() {
       exit 1;
     fi
 
+    local privileges_property="FINERACT_DATABASE_USER"${username_uppercase}"_PRIVILEGES"
+    local privileges=${!privileges_property}
+
+    if [ -z $privileges ]; then
+      echo "Privileges not found for user $user"
+      exit 1;
+    fi
+
     check_and_create_userrole $user $password
+    assign_privileges $user $privileges
   done
 
 }
@@ -33,16 +42,21 @@ check_and_create_userrole() {
 
   user_created_flag="$(export PGPASSWORD=$POSTGRES_PASSWORD; psql -Atq -h $POSTGRES_HOSTNAME -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "SELECT 1 FROM PG_CATALOG.PG_ROLES WHERE ROLNAME = '$username';")"
     if [ "$user_created_flag" = 1 ]; then
-        echo "Database Role $username already created. Skipping... \n"
+        echo "Database Role $username already created. Skipping..."
     else
       echo "Creating Role $username"
       export PGPASSWORD=$POSTGRES_PASSWORD;
       psql -h $POSTGRES_HOSTNAME -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE USER $username WITH PASSWORD '$password';"
-      echo "Role $username created. \n"
+      echo "Role $username created."
     fi
+}
+
+assign_privileges() {
+    local username=$1
+    local privileges=$2
 }
 
 #Create DB users
 check_and_create_userroles
 
-echo "----------Database Setup Complete---------- \n"
+echo "----------Database Setup Complete----------"
